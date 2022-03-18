e9370ac080685e2c2b24612e9933e1c87f584887
rkmv8 <rickardkarlmartin@gmail.com>
2022-03-18 14:41:09 +0100

    Minor refactoring

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-const generatedQuotes = [];
-
-
 async function fetchText() {
     let response = await fetch('https://api.kanye.rest');
     let data = await response.json();
     document.getElementById('quotetext').innerHTML = "\""+data.quote+"\"";;
-    generatedQuotes.push(data.quote);
-    console.log(generatedQuotes)
 }
